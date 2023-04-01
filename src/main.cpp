f712330343a532a5837ed366a6dfc6038bd51ba0
DevChew <the@devchew.com>
2023-04-01 13:04:03 +0200

    http setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,77 @@
 #include <Arduino.h>
+#ifdef ESP32
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#endif
+#include <ESPAsyncWebServer.h>
+#include <LittleFS.h>
 
-void setup() {
-  // put your setup code here, to run once:
+/*
+Replace the SSID and Password according to your wifi
+*/
+const char *ssid = "slysze_jak_pierdzisz";
+const char *password = "ModaNaSukces11";
+
+// Webserver
+AsyncWebServer server(80);
+
+void setupSerial() {
+  Serial.begin(115200);
+  Serial.println("Starting the LittleFS Webserver..");
+}
+
+void setupFS() {
+    // Begin LittleFS
+    if (!LittleFS.begin()) {
+        Serial.println("An Error has occurred while mounting LittleFS");
+        return;
+    }
+}
+
+void notFound(AsyncWebServerRequest *request)
+{
+    request->send(404, "text/plain", "Not found");
+}
+
+void setupHttp()
+{
+
+    // Connect to WIFI
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    if (WiFi.waitForConnectResult() != WL_CONNECTED)
+    {
+        Serial.printf("WiFi Failed!\n");
+        return;
+    }
+
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    // Route for root index.html
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(LittleFS, "/index.html", "text/html"); });
+
+    // Route for root styles.css
+    server.on("/styles.css", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(LittleFS, "/styles.css", "text/css"); });
+    // Route for root index.js
+    server.on("/index.js", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(LittleFS, "/index.js", "text/javascript"); });
+
+    server.onNotFound(notFound);
+
+    server.begin();
+}
+
+void setup(){
+  setupSerial();
+  setupFS();
+  setupHttp();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
